92082748f57c14cff0724ce3fe8b9b2d65c67473
wujindong <498298758@qq.com>
2017-12-19 13:48:47 +0800

    ''

---
+++ PHP/EcsController.class.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * 16进制加解密
+ */
+class EcsController{
+    public $key ="";
+    public function __construct(){
+        $key="rZ9hubPsFke5TLIrZbMClfyGUPfqaP0pysQEedvE";
+        $this->key=substr($key,0,8);
+    }
+
+    //数据加密
+    public function encrypt($input)
+    {
+        $size = mcrypt_get_block_size(MCRYPT_3DES,'ecb');
+        $input = $this->pkcs5_pad($input, $size);
+        $key = str_pad($this->key,24,'0');
+        $td = mcrypt_module_open(MCRYPT_3DES, '', 'ecb', '');
+        $iv = @mcrypt_create_iv (mcrypt_enc_get_iv_size($td), MCRYPT_RAND);
+        @mcrypt_generic_init($td, $key, $iv);
+        $data = mcrypt_generic($td, $input);
+        mcrypt_generic_deinit($td);
+        mcrypt_module_close($td);
+        $data = bin2hex($data);
+        return $data;
+    }
+
+    //数据解密
+    public function decrypt($encrypted)
+    {
+        $encrypted =hex2bin($encrypted);
+        $key = str_pad($this->key,24,'0');
+        $td = mcrypt_module_open(MCRYPT_3DES,'','ecb','');
+        $iv = @mcrypt_create_iv(mcrypt_enc_get_iv_size($td),MCRYPT_RAND);
+        $ks = mcrypt_enc_get_key_size($td);
+        @mcrypt_generic_init($td, $key, $iv);
+        $decrypted = mdecrypt_generic($td, $encrypted);
+        mcrypt_generic_deinit($td);
+        mcrypt_module_close($td);
+        $y=$this->pkcs5_unpad($decrypted);
+        return $y;
+    }
+
+    public function pkcs5_pad ($text, $blocksize)
+    {
+        $pad = $blocksize - (strlen($text) % $blocksize);
+        return $text . str_repeat(chr($pad), $pad);
+    }
+
+    public function pkcs5_unpad($text)
+    {
+        $pad = ord($text{strlen($text)-1});
+        if ($pad > strlen($text))
+        {
+            return false;
+        }
+        if (strspn($text, chr($pad), strlen($text) - $pad) != $pad)
+        {
+            return false;
+        }
+        return substr($text, 0, -1 * $pad);
+    }
+
+}
